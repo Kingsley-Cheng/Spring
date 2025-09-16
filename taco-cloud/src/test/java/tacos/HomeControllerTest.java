306048ff9fb986bf3c2a9dd2eb633cbfe3150ca5
Kingsley-Cheng <chengzijie2021@outlook.com>
2025-09-16 09:28:51 +0800

    2025-09-16

---
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package tacos;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class HomeControllerTest {
+    private MockMvc mockMvc;
+
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/")).
+                andExpect(status().isOk()).
+                andExpect(view().name("home")).
+                andExpect(content().string(containsString("Welcome to...")));
+    }
+}
